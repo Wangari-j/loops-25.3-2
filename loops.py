1e54b7bd8eeaeb2b40013b125eab6b4ba205d90e
Wangari-j <wangarijudy@gmail.com>
2024-03-25 05:52:08 +0300

    loops

---
+++ loops.py
@@ -0,0 +1,18 @@
+#loop through a string
+
+lang = 'python'
+
+for y in lang:
+    print(y)
+
+#python with for loop range
+    
+values = range(4)
+
+for i in range(4):
+    print(i)
+
+    
+
+for b in range(0, 10, 2):
+    print(b)
